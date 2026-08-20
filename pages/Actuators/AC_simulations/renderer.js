fdf049c81ee8e0a72b12bbbf932a8a44954cf440
Suyash Gautam <gautamsuyash5@gmail.com>
2026-08-20 14:52:41 +0530

    AC Simulation

--- pages/Actuators/AC_simulations/renderer.js
+++ pages/Actuators/AC_simulations/renderer.js
@@ -69,13 +69,13 @@ function drawLines() {
   document.getElementById('horizontalLine3').setAttribute('y2', circuitM);
 
   document.getElementById('neutralCircle').setAttribute('cx', smpsPositionXL);
-  document.getElementById('neutralCircle').setAttribute('cy', powerButtonY - 25);
+  document.getElementById('neutralCircle').setAttribute('cy', powerButtonY - 50);
 
   document.getElementById('liveCircle').setAttribute('cx', smpsPositionXR);
-  document.getElementById('liveCircle').setAttribute('cy', powerButtonY - 25);
+  document.getElementById('liveCircle').setAttribute('cy', powerButtonY - 50);
 
   document.getElementById('liveCircle2').setAttribute('cx', smpsPositionXL + (smpsPositionXR - smpsPositionXL)/2);
-  document.getElementById('liveCircle2').setAttribute('cy', powerButtonY - 25);
+  document.getElementById('liveCircle2').setAttribute('cy', powerButtonY - 50);
   
   document.getElementById('main-smpsN').setAttribute('x', smpsPositionXL - 5);
   document.getElementById('main-smpsN').setAttribute('y', powerButtonY - 90);
